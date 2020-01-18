b519e7988ed03dcfa6d13710562a2536922047c0
Mairi Ng <mairi.ng.16@ucl.ac.uk>
2020-01-18 13:18:00 +0000

    fixed navbar styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './nav-bar.scss';
+import CardGroup from '../InfoCardGroup/InfoCardGroup';
 
 
 class NavBar extends Component {
     render() {
         return (
-            <nav>
-                <ul>
-                    <li><Link to='/'>LIVING TOGETHER</Link></li>
-                    <li><Link to='/managePeople'>Manage People</Link></li>
-                    <li><Link to="/manageInfo">Manage Info</Link></li>
-                </ul>
-            </nav>
+            <header>
+                <div className="logo"><Link to='/'>LIVING TOGETHER</Link></div>
+                <nav>
+                    <ul>
+                        <li><Link to='/managePeople'>Manage People</Link></li>
+                        <li><Link to="/manageInfo">Manage Info</Link></li>
+                    </ul>
+                </nav>
+            </header>
         );
     }
 }
